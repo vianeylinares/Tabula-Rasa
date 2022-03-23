72641ee41c803e9c1776a9061f5d61214256fcb8
vianey <vianeylinares@gmail.com>
2022-03-23 13:00:13 -0700

    Added shortcode function for frontend seat assignment sections display. Function includes logic for user login status.

--- frontend/seats-assignment.php
+++ frontend/seats-assignment.php
@@ -4,3 +4,40 @@
 if(!defined('ABSPATH')){
        exit;
 }
+
+
+function tabula_rasa_seats_assignment_shortcode(){
+
+	ob_start();
+
+	if ( is_user_logged_in() ) {
+
+		
+
+	} else {
+
+		$args = array(
+			'echo' => true,
+			'redirect' => '',
+			'form_id' => 'seat-loginform',
+			'label_username' => __('Username'),
+			'label_password' => __('Password'),
+			'label_remember' => __('Remember Me'),
+			'label_log_in' => __('Log In'),
+			'id_username' => 'user_login',
+			'id_password' => 'user_pass',
+			'id_remember' => 'rememberme',
+			'id_submit' => 'wp-submit',
+			'remember' => true,
+			'value_username' => NULL,
+			'value_remember' => true,
+		);
+
+	    wp_login_form($args);
+
+	}
+
+	return ob_get_clean();
+
+}
+add_shortcode('seats_assignment', 'tabula_rasa_seats_assignment_shortcode');
