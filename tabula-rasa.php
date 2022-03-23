ae6a4c86ae9261ce3751dc63ea4310e35dcf9058
vianey <vianeylinares@gmail.com>
2022-03-23 12:39:39 -0700

    Initial WP plugin setup.

---
+++ tabula-rasa.php
@@ -0,0 +1,15 @@
+<?php
+/*
+ * Plugin name: Tabula Rasa (Experimental)
+ * Plugin URI: Nuestra-Boda.com.mx
+ * Description: Wedding guests seat assignment
+ * Author: Nuestra-Boda
+ * Version: 1.0.0
+ * License: Open
+ * 
+ */
+
+//Exit if accessed directly
+if(!defined('ABSPATH')){
+       exit;
+}
