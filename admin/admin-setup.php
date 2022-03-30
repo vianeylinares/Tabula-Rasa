701b9da74dda37132bf775e588bfea2642533c0c
vianey <vianeylinares@gmail.com>
2022-03-29 17:07:42 -0700

    Added styles and functions for preliminary table types display at 'Table settings' page in admin.

--- admin/admin-setup.php
+++ admin/admin-setup.php
@@ -14,15 +14,51 @@ function tabularasa_add_settings_page() {
 add_action( 'admin_menu', 'tabularasa_add_settings_page' );
 
 
+function tabularasa_admin_css_and_js(){
+
+	$plugin_url = plugin_dir_url( __FILE__ );
+
+    wp_enqueue_style( 'admin-style', $plugin_url . '../admin/css/style.css' );
+
+}
+add_action( 'admin_enqueue_scripts', 'tabularasa_admin_css_and_js' );
+
+
 function tabularasa_render_plugin_settings_page() {
 
     ?>
 
     <h1>Tabula Rasa - Settings</h1>
 
-    <h2>Table setup</h2>
+    <div class="table-types-box">
 
-    
+		<h2>Table types</h2>
+
+		<div class="table-types-space clearfix">
+
+			<div class="table-box" id="table-rectangular-10-seat">
+				<img src="<?php echo plugin_dir_url( __FILE__ ); ?>../images/table-rectangular-10-seat-0.png" />
+			</div>
+
+			<div class="table-box" id="table-round-10-seat">
+				<img src="<?php echo plugin_dir_url( __FILE__ ); ?>../images/table-round-10-seat-0.png" />
+			</div>
+
+			<div class="table-box" id="table-round-8-seat">
+				<img src="<?php echo plugin_dir_url( __FILE__ ); ?>../images/table-round-8-seat-0.png" />
+			</div>
+
+			<div class="table-box" id="table-round-9-seat">
+				<img src="<?php echo plugin_dir_url( __FILE__ ); ?>../images/table-round-9-seat-0.png" />
+			</div>
+
+			<div class="table-box" id="table-squared-4-seat">
+				<img src="<?php echo plugin_dir_url( __FILE__ ); ?>../images/table-squared-4-seat-0.png" />
+			</div>
+
+		</div>
+
+	</div>
 
 	<?php
 
