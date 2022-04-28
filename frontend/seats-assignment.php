f296d6075a6b5a853871343daaa5cf7b4bcda26a
vianey <vianeylinares@gmail.com>
2022-04-27 19:02:35 -0500

    Added shortcode function and style for Group View at frontend.

--- frontend/seats-assignment.php
+++ frontend/seats-assignment.php
@@ -548,11 +548,145 @@ function tabula_rasa_table_view_shortcode(){
 add_shortcode( 'table_view', 'tabula_rasa_table_view_shortcode' );
 
 
+function tabula_rasa_group_view_shortcode(){
+
+	ob_start();
+
+	if ( is_user_logged_in() ) {
+
+		?>
+
+			<div class="assignment-wrapper clearfix">
+			    
+			    <?php
+
+					$args = array(
+							'post_type' => 'guest',
+							'order' => 'ASC',
+							'posts_per_page' => -1,
+						);
+
+						$query = new WP_Query($args);
+
+						$count_draggs = 1;
+
+						if ( $query->have_posts() ) :
+						
+							while($query -> have_posts()) : $query -> the_post();
+
+								$count_guest = 1;	
+
+								if( get_post_meta(get_the_ID(), 'status', true) == 1 && get_post_meta(get_the_ID(), 'assigned_table', true) != 0){
+
+									?>
+
+										<div class="report-group">
+										    
+										    <div class="table-title" style="margin-bottom: 30px;"><?php echo get_the_title(); ?>'s group</div>
+
+											<div id="user-<?php echo get_the_ID(); ?>" class="table-list">
+												<input type="checkbox" value="<?php echo get_the_ID(); ?>" <?php echo (get_post_meta(get_the_ID(), 'arrived', true))? "checked" : "" ; ?> /> 
+												<span <?php echo (get_post_meta(get_the_ID(), 'arrived', true))? "class='arrived'" : "" ; ?>>
+                        				            <b><?php echo get_the_title(); ?></b>
+                        				        </span>
+                        				        <?php $table_num = (int)get_post_meta(get_the_ID(), 'assigned_table', true); ?>
+                        				        (Table: <?php echo get_post_meta($table_num, 'table_num', true); ?>)
+											</div>
+
+											<?php
+
+												$main_guest = get_the_ID();
+
+												$args2 = array(
+													'post_type' => 'guest',
+													'order' => 'ASC',
+													'posts_per_page' => '-1',
+													'meta_key' => 'with_main_guest',
+							    					'meta_value' =>  $main_guest,
+												);
+
+												$query2 = new WP_Query($args2);
+
+												if ( $query2->have_posts() ) :
+									
+													while($query2 -> have_posts()) : $query2 -> the_post();
+
+														if( get_post_meta(get_the_ID(), 'assigned_table', true) != 0 ){
+
+															?>
+																<div id="user-<?php echo get_the_ID(); ?>" class="table-list">
+                    												<input type="checkbox" value="<?php echo get_the_ID(); ?>" <?php echo (get_post_meta(get_the_ID(), 'arrived', true))? "checked" : "" ; ?> /> 
+                    												<span <?php echo (get_post_meta(get_the_ID(), 'arrived', true))? "class='arrived'" : "" ; ?>>
+                                            				            <?php echo get_the_title(); ?>
+                                            				        </span>
+                                            				        <?php $table_num = (int)get_post_meta(get_the_ID(), 'assigned_table', true); ?>
+                        				                            (Table: <?php echo get_post_meta($table_num, 'table_num', true); ?>)
+                    											</div>
+
+															<?php
+
+														}
+
+													endwhile;				
+
+												endif; wp_reset_postdata();
+
+											?>
+
+										</div>
+
+									<?php
+
+								}
+
+							endwhile;
+
+						else:
+
+						    echo 'no guests found';
+
+						endif; wp_reset_postdata();
+
+				?>
+			    
+			</div>
+
+		<?php
+
+	} else {
+
+		$args = array(
+			'echo' => true,
+			'redirect' => '',
+			'form_id' => 'seat-loginform',
+			'label_username' => __('Username'),
+			'label_password' => __('Password'),
+			'label_remember' => __('Remember Me'),
+			'label_log_in' => __('Log In'),
+			'id_username' => 'user_login',
+			'id_password' => 'user_pass',
+			'id_remember' => 'rememberme',
+			'id_submit' => 'wp-submit',
+			'remember' => true,
+			'value_username' => NULL,
+			'value_remember' => true,
+		);
+
+	    wp_login_form($args);
+
+	}
+
+	return ob_get_clean();
+
+}
+add_shortcode( 'group_view', 'tabula_rasa_group_view_shortcode' );
+
+
 function tabula_rasa_seats_assignment_js(){
 
 	global $post;
 
-	if(has_shortcode( $post->post_content, 'seats_assignment' ) || has_shortcode( $post->post_content, 'table_view' )){
+	if(has_shortcode( $post->post_content, 'seats_assignment' ) || has_shortcode( $post->post_content, 'table_view' ) || has_shortcode( $post->post_content, 'group_view' )){
 
 		?>
 
